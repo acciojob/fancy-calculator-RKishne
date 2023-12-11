fce2b8b58e0f174438df22362f96321a25f59aed
Rahul Kishne <kishnerahul123@gmail.com>
2023-12-11 14:28:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const firstno=parseInt(prompt("Enter first number"));
+const multipliedno=firstno*2;
+
+const secondno=parseInt(prompt("Enter second no."));
+
+const finalresult=multipliedno+secondno;
+
+alert("The final result is: "+finalresult);
